f9cf441616a66e2c4eb50504965e8ad871284be0
joaolucasalves010 <ylucasslalves@gmail.com>
2024-03-27 18:39:33 -0300

    fazendo senha aleatoria

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // seleção de elementos
 const generatePasswordButton = document.querySelector("#generate-password")
 const generatedPasswordElement = document.querySelector("#generated-password")
-
+const passwordElement = document.querySelector("#generated-password h4")
 // funções
 const gettLetterLowerCase = () => {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
@@ -21,5 +21,45 @@ const getSymbol = () => {
   return symbols[randomSymbol]
 }
 
+const generatedPassword = (
+  gettLetterLowerCase,
+  getLetterUpperCase,
+  getNumber,
+  getSymbol
+) => {
+  let password = ""
+
+  const passwordLenght = 10
+
+  const generators = [
+    gettLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol,
+  ]
+
+  for (i = 0; i < passwordLenght; i = i + 4) {
+    generators.forEach(() => {
+      let randomValue =
+        generators[Math.floor(Math.random() * generators.length)]()
+
+      password = password + randomValue
+    })
+  }
+  password = password.slice(0, passwordLenght)
+
+  passwordElement.innerHTML = password
+  generatedPasswordElement.style.display = "block"
+
+  return password
+}
+
 //eventos
-generatePasswordButton.addEventListener("click", () => {})
+generatePasswordButton.addEventListener("click", () => {
+  const password = generatedPassword(
+    gettLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol
+  )
+})
